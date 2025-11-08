cda887c867b9b1569e41bea5d59bf2f47c1e60f6
FalconFelipe <halcondorado123@live.com>
2025-11-07 19:10:08 -0500

    Ultimas limpiezas en codigo

--- RJAS-Market_App/Proveedor.Designer.cs
+++ RJAS-Market_App/Proveedor.Designer.cs
@@ -15,6 +15,7 @@
 
         #region Windows Form Designer generated code
 
+        // Método necesario para admitir el Diseñador. No se puede modificar
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Proveedor));
@@ -61,9 +62,8 @@
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             panelBotones.SuspendLayout();
             SuspendLayout();
-            // 
+
             // panelHeader
-            // 
             panelHeader.BackColor = Color.FromArgb(142, 68, 173);
             panelHeader.Controls.Add(lblTitulo);
             panelHeader.Controls.Add(pictureBoxLogo);
@@ -73,9 +73,8 @@
             panelHeader.Name = "panelHeader";
             panelHeader.Size = new Size(1516, 92);
             panelHeader.TabIndex = 0;
-            // 
-            // lblTitulo
-            // 
+
+            // lblTitulo 
             lblTitulo.AutoSize = true;
             lblTitulo.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
             lblTitulo.ForeColor = Color.White;
@@ -85,9 +84,8 @@
             lblTitulo.Size = new Size(367, 37);
             lblTitulo.TabIndex = 1;
             lblTitulo.Text = "GESTIÓN DE PROVEEDORES";
-            // 
-            // pictureBoxLogo
-            // 
+
+            // pictureBoxLogo 
             pictureBoxLogo.BackColor = Color.White;
             pictureBoxLogo.Image = (Image)resources.GetObject("pictureBoxLogo.Image");
             pictureBoxLogo.Location = new Point(24, 11);
@@ -97,9 +95,8 @@
             pictureBoxLogo.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBoxLogo.TabIndex = 0;
             pictureBoxLogo.TabStop = false;
-            // 
+
             // panelDatosProveedor
-            // 
             panelDatosProveedor.BackColor = Color.White;
             panelDatosProveedor.BorderStyle = BorderStyle.FixedSingle;
             panelDatosProveedor.Controls.Add(lblSeccionProveedor);
@@ -116,9 +113,8 @@
             panelDatosProveedor.Name = "panelDatosProveedor";
             panelDatosProveedor.Size = new Size(677, 276);
             panelDatosProveedor.TabIndex = 1;
-            // 
+
             // lblSeccionProveedor
-            // 
             lblSeccionProveedor.AutoSize = true;
             lblSeccionProveedor.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
             lblSeccionProveedor.ForeColor = Color.FromArgb(142, 68, 173);
@@ -128,9 +124,8 @@
             lblSeccionProveedor.Size = new Size(192, 21);
             lblSeccionProveedor.TabIndex = 0;
             lblSeccionProveedor.Text = "📋 Datos del Proveedor";
-            // 
+
             // nombreproveedorlbl
-            // 
             nombreproveedorlbl.AutoSize = true;
             nombreproveedorlbl.Font = new Font("Segoe UI", 9F);
             nombreproveedorlbl.ForeColor = Color.FromArgb(52, 73, 94);
@@ -140,9 +135,8 @@
             nombreproveedorlbl.Size = new Size(116, 15);
             nombreproveedorlbl.TabIndex = 1;
             nombreproveedorlbl.Text = "Nombre Proveedor *";
-            // 
+
             // nombreProveedorTxt
-            // 
             nombreProveedorTxt.Font = new Font("Segoe UI", 10F);
             nombreProveedorTxt.Location = new Point(24, 86);
             nombreProveedorTxt.Margin = new Padding(4, 4, 4, 4);
@@ -150,9 +144,8 @@
             nombreProveedorTxt.Size = new Size(630, 25);
             nombreProveedorTxt.TabIndex = 2;
             nombreProveedorTxt.TextChanged += textBox1_TextChanged;
-            // 
+
             // categorialbl
-            // 
             categorialbl.AutoSize = true;
             categorialbl.Font = new Font("Segoe UI", 9F);
             categorialbl.ForeColor = Color.FromArgb(52, 73, 94);
@@ -163,9 +156,8 @@
             categorialbl.TabIndex = 3;
             categorialbl.Text = "Categoría *";
             categorialbl.Click += categorialbl_Click;
-            // 
+
             // categoriaCmb
-            // 
             categoriaCmb.Font = new Font("Segoe UI", 10F);
             categoriaCmb.FormattingEnabled = true;
             categoriaCmb.Location = new Point(24, 150);
@@ -173,9 +165,8 @@
             categoriaCmb.Name = "categoriaCmb";
             categoriaCmb.Size = new Size(630, 25);
             categoriaCmb.TabIndex = 4;
-            // 
+
             // telefonolbl
-            // 
             telefonolbl.AutoSize = true;
             telefonolbl.Font = new Font("Segoe UI", 9F);
             telefonolbl.ForeColor = Color.FromArgb(52, 73, 94);
@@ -185,9 +176,8 @@
             telefonolbl.Size = new Size(52, 15);
             telefonolbl.TabIndex = 5;
             telefonolbl.Text = "Teléfono";
-            // 
+
             // telefonoTxt
-            // 
             telefonoTxt.Font = new Font("Segoe UI", 10F);
             telefonoTxt.Location = new Point(24, 214);
             telefonoTxt.Margin = new Padding(4, 4, 4, 4);
@@ -195,9 +185,8 @@
             telefonoTxt.Size = new Size(302, 25);
             telefonoTxt.TabIndex = 6;
             telefonoTxt.TextChanged += telefonoTxt_TextChanged;
-            // 
+
             // emaillbl
-            // 
             emaillbl.AutoSize = true;
             emaillbl.Font = new Font("Segoe UI", 9F);
             emaillbl.ForeColor = Color.FromArgb(52, 73, 94);
@@ -207,18 +196,16 @@
             emaillbl.Size = new Size(36, 15);
             emaillbl.TabIndex = 7;
             emaillbl.Text = "Email";
-            // 
+
             // emailTxt
-            // 
             emailTxt.Font = new Font("Segoe UI", 10F);
             emailTxt.Location = new Point(350, 214);
             emailTxt.Margin = new Padding(4, 4, 4, 4);
             emailTxt.Name = "emailTxt";
             emailTxt.Size = new Size(302, 25);
             emailTxt.TabIndex = 8;
-            // 
+
             // panelDatosProducto
-            // 
             panelDatosProducto.BackColor = Color.White;
             panelDatosProducto.BorderStyle = BorderStyle.FixedSingle;
             panelDatosProducto.Controls.Add(lblSeccionProducto);
@@ -240,9 +227,8 @@
             panelDatosProducto.Name = "panelDatosProducto";
             panelDatosProducto.Size = new Size(769, 276);
             panelDatosProducto.TabIndex = 2;
-            // 
+
             // lblSeccionProducto
-            // 
             lblSeccionProducto.AutoSize = true;
             lblSeccionProducto.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
             lblSeccionProducto.ForeColor = Color.FromArgb(142, 68, 173);
@@ -252,9 +238,8 @@
             lblSeccionProducto.Size = new Size(183, 21);
             lblSeccionProducto.TabIndex = 0;
             lblSeccionProducto.Text = "📦 Datos del Producto";
-            // 
+
             // descripcionproductolbl
-            // 
             descripcionproductolbl.AutoSize = true;
             descripcionproductolbl.Font = new Font("Segoe UI", 9F);
             descripcionproductolbl.ForeColor = Color.FromArgb(52, 73, 94);
@@ -265,18 +250,16 @@
             descripcionproductolbl.TabIndex = 1;
             descripcionproductolbl.Text = "Descripción Producto *";
             descripcionproductolbl.Click += label1_Click_1;
-            // 
+
             // comentarioTxt
-            // 
             comentarioTxt.Font = new Font("Segoe UI", 10F);
             comentarioTxt.Location = new Point(24, 86);
             comentarioTxt.Margin = new Padding(4, 4, 4, 4);
             comentarioTxt.Name = "comentarioTxt";
             comentarioTxt.Size = new Size(722, 25);
             comentarioTxt.TabIndex = 2;
-            // 
+
             // cantidadlbl
-            // 
             cantidadlbl.AutoSize = true;
             cantidadlbl.Font = new Font("Segoe UI", 9F);
             cantidadlbl.ForeColor = Color.FromArgb(52, 73, 94);
@@ -287,9 +270,8 @@
             cantidadlbl.TabIndex = 3;
             cantidadlbl.Text = "Cantidad *";
             cantidadlbl.Click += label1_Click_2;
-            // 
+
             // cantidadTxt
-            // 
             cantidadTxt.Font = new Font("Segoe UI", 10F);
             cantidadTxt.Location = new Point(24, 150);
             cantidadTxt.Margin = new Padding(4, 4, 4, 4);
@@ -297,9 +279,8 @@
             cantidadTxt.Size = new Size(175, 25);
             cantidadTxt.TabIndex = 4;
             cantidadTxt.TextChanged += cantidadTxt_TextChanged;
-            // 
+
             // preciolbl
-            // 
             preciolbl.AutoSize = true;
             preciolbl.Font = new Font("Segoe UI", 9F);
             preciolbl.ForeColor = Color.FromArgb(52, 73, 94);
@@ -309,9 +290,8 @@
             preciolbl.Size = new Size(48, 15);
             preciolbl.TabIndex = 5;
             preciolbl.Text = "Precio *";
-            // 
+
             // precioTxt
-            // 
             precioTxt.Font = new Font("Segoe UI", 10F);
             precioTxt.Location = new Point(221, 150);
             precioTxt.Margin = new Padding(4, 4, 4, 4);
@@ -319,9 +299,8 @@
             precioTxt.Size = new Size(175, 25);
             precioTxt.TabIndex = 6;
             precioTxt.TextChanged += precioTxt_TextChanged;
-            // 
+
             // Impuesto19lbl
-            // 
             Impuesto19lbl.AutoSize = true;
             Impuesto19lbl.Font = new Font("Segoe UI", 9F);
             Impuesto19lbl.ForeColor = Color.FromArgb(52, 73, 94);
@@ -331,9 +310,8 @@
             Impuesto19lbl.Size = new Size(82, 15);
             Impuesto19lbl.TabIndex = 7;
             Impuesto19lbl.Text = "Impuesto 19%";
-            // 
+
             // impuestoTxt
-            // 
             impuestoTxt.BackColor = Color.FromArgb(241, 242, 246);
             impuestoTxt.Font = new Font("Segoe UI", 10F);
             impuestoTxt.Location = new Point(24, 214);
@@ -343,9 +321,8 @@
             impuestoTxt.Size = new Size(175, 25);
             impuestoTxt.TabIndex = 8;
             impuestoTxt.TextChanged += textBox7_TextChanged;
-            // 
+
             // descuentolbl
-            // 
             descuentolbl.AutoSize = true;
             descuentolbl.Font = new Font("Segoe UI", 9F);
             descuentolbl.ForeColor = Color.FromArgb(52, 73, 94);
@@ -355,18 +332,16 @@
             descuentolbl.Size = new Size(63, 15);
             descuentolbl.TabIndex = 9;
             descuentolbl.Text = "Descuento";
-            // 
+
             // descuentoTxt
-            // 
             descuentoTxt.Font = new Font("Segoe UI", 10F);
             descuentoTxt.Location = new Point(221, 214);
             descuentoTxt.Margin = new Padding(4, 4, 4, 4);
             descuentoTxt.Name = "descuentoTxt";
             descuentoTxt.Size = new Size(175, 25);
             descuentoTxt.TabIndex = 10;
-            // 
+
             // activoRdbt
-            // 
             activoRdbt.AutoSize = true;
             activoRdbt.Font = new Font("Segoe UI", 9F);
             activoRdbt.ForeColor = Color.FromArgb(52, 73, 94);
@@ -378,9 +353,8 @@
             activoRdbt.TabStop = true;
             activoRdbt.Text = "¿Activo?";
             activoRdbt.UseVisualStyleBackColor = true;
-            // 
+
             // totallbl
-            // 
             totallbl.AutoSize = true;
             totallbl.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
             totallbl.ForeColor = Color.FromArgb(52, 73, 94);
@@ -390,9 +364,8 @@
             totallbl.Size = new Size(42, 19);
             totallbl.TabIndex = 12;
             totallbl.Text = "Total";
-            // 
+
             // totalTxt
-            // 
             totalTxt.BackColor = Color.FromArgb(241, 242, 246);
             totalTxt.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
             totalTxt.ForeColor = Color.FromArgb(142, 68, 173);
@@ -403,9 +376,8 @@
             totalTxt.Size = new Size(210, 27);
             totalTxt.TabIndex = 13;
             totalTxt.TextAlign = HorizontalAlignment.Right;
-            // 
+
             // panelDataGrid
-            // 
             panelDataGrid.BackColor = Color.White;
             panelDataGrid.BorderStyle = BorderStyle.FixedSingle;
             panelDataGrid.Controls.Add(dataGridView1);
@@ -414,9 +386,8 @@
             panelDataGrid.Name = "panelDataGrid";
             panelDataGrid.Size = new Size(1469, 323);
             panelDataGrid.TabIndex = 3;
-            // 
+
             // dataGridView1
-            // 
             dataGridView1.AllowUserToAddRows = false;
             dataGridView1.AllowUserToDeleteRows = false;
             dataGridView1.BackgroundColor = Color.White;
@@ -432,9 +403,8 @@
             dataGridView1.Size = new Size(1467, 321);
             dataGridView1.TabIndex = 0;
             dataGridView1.CellContentClick += dataGridView1_CellClick;
-            // 
+
             // panelBotones
-            // 
             panelBotones.BackColor = Color.White;
             panelBotones.Controls.Add(CargarBtn);
             panelBotones.Controls.Add(actualizarBtn);
@@ -445,9 +415,8 @@
             panelBotones.Name = "panelBotones";
             panelBotones.Size = new Size(1470, 69);
             panelBotones.TabIndex = 4;
-            // 
+
             // CargarBtn
-            // 
             CargarBtn.BackColor = Color.FromArgb(46, 204, 113);
             CargarBtn.Cursor = Cursors.Hand;
             CargarBtn.FlatAppearance.BorderSize = 0;
@@ -462,9 +431,8 @@
             CargarBtn.Text = "💾 Cargar";
             CargarBtn.UseVisualStyleBackColor = false;
             CargarBtn.Click += CargarBtn_Click;
-            // 
+
             // actualizarBtn
-            // 
             actualizarBtn.BackColor = Color.FromArgb(52, 152, 219);
             actualizarBtn.Cursor = Cursors.Hand;
             actualizarBtn.FlatAppearance.BorderSize = 0;
@@ -479,9 +447,8 @@
             actualizarBtn.Text = "🔄 Actualizar";
             actualizarBtn.UseVisualStyleBackColor = false;
             actualizarBtn.Click += button1_Click;
-            // 
+
             // limpiarBtn
-            // 
             limpiarBtn.BackColor = Color.FromArgb(149, 165, 166);
             limpiarBtn.Cursor = Cursors.Hand;
             limpiarBtn.FlatAppearance.BorderSize = 0;
@@ -495,9 +462,8 @@
             limpiarBtn.TabIndex = 2;
             limpiarBtn.Text = "🗑️ Limpiar";
             limpiarBtn.UseVisualStyleBackColor = false;
-            // 
+
             // finalizarBtn
-            // 
             finalizarBtn.BackColor = Color.FromArgb(142, 68, 173);
             finalizarBtn.Cursor = Cursors.Hand;
             finalizarBtn.FlatAppearance.BorderSize = 0;
@@ -512,9 +478,8 @@
             finalizarBtn.Text = "✅ Finalizar";
             finalizarBtn.UseVisualStyleBackColor = false;
             finalizarBtn.Click += finalizarBtn_Click;
-            // 
+
             // Proveedor
-            // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(236, 240, 241);
